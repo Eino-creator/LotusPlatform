81e4f12cf71997bf78d1af7ceb7f63879506d4fa
Eino Hummel <ehummel8358@student.landstede.nl>
2025-03-03 09:06:32 +0100

    Add react-responsive-carousel dependency and implement carousel in Welcome page

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,31 +1,30 @@
 import '/resources/css/Welcome.css';
 import GuestLayout from '@/Layouts/GuestLayout';
 import { Head, Link } from '@inertiajs/react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 export default function Welcome() {
-    // const handleImageError = () => {
-    //     document
-    //         .getElementById('screenshot-container')
-    //         ?.classList.add('!hidden');
-    //     document.getElementById('docs-card')?.classList.add('!row-span-1');
-    //     document
-    //         .getElementById('docs-card-content')
-    //         ?.classList.add('!flex-row');
-    //     document.getElementById('background')?.classList.add('!hidden');
-    // };
-
     return (
         <GuestLayout>
             <Head title="LotusKring Zuid-Oost Drenthe" />
             <div className="welcome-container">
                 <div>
-                    <div>
-                        <main>
-                            <div className="mt-6">
-                                dit is de welkom pagina voor lotuskring Zuid-Oost Drenthe
-                            </div>
-                        </main>
-                    </div>
+                    <main>
+                        <section className="image-carrusel">
+                            <Carousel showThumbs={false} autoPlay infiniteLoop>
+                                <div>
+                                    <img src="/img/banner-placeholder.jpg" alt="LotusKring Zuid-Oost Drenthe" />
+                                </div>
+                                <div>
+                                    <img src="/img/another-image.jpg" alt="Another Image" />
+                                </div>
+                                <div>
+                                    <img src="/img/yet-another-image.jpg" alt="Yet Another Image" />
+                                </div>
+                            </Carousel>
+                        </section>
+                    </main>
                 </div>
             </div>
         </GuestLayout>
