599b1bed60bff5981c12a6b30710fb4a12335644
Eino Hummel <ehummel8358@student.landstede.nl>
2025-03-05 10:55:10 +0100

    Add welcome section with text and map to Welcome page; update carousel component

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -11,18 +11,45 @@ export default function Welcome() {
             <div className="welcome-container">
                 <div>
                     <main>
-                        <section className="image-carrusel">
-                            <Carousel showThumbs={false} autoPlay infiniteLoop>
+                        <section className="image-carousel">
+                            <Carousel showThumbs={false} autoPlay infiniteLoop interval={5000}>
                                 <div>
                                     <img src="/img/banner-placeholder.jpg" alt="LotusKring Zuid-Oost Drenthe" />
                                 </div>
                                 <div>
-                                    <img src="/img/another-image.jpg" alt="Another Image" />
+                                    <img src="/img/banner-placeholder.jpg" alt="Another Image" />
                                 </div>
                                 <div>
-                                    <img src="/img/yet-another-image.jpg" alt="Yet Another Image" />
+                                    <img src="/img/banner-placeholder.jpg" alt="Yet Another Image" />
                                 </div>
                             </Carousel>
+                        </section>
+                        <section className='welcome-content'>
+                            <div className='welcome-text'>
+                                <div className='welcome-text-content'>
+                                    <h1>Welkom bij LotusKring Zuid-Oost Drenthe</h1>
+                                    <p>
+                                        iorum ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus
+                                        euismod, ultricies mi at, tincidunt neque. Sed nec lacus eget odio
+                                        fermentum tincidunt. Nulla facilisi. Aenean nec enim nec nunc
+                                        condimentum ultricies. Sed nec lacus eget odio fermentum tincidunt.
+                                        Nulla facilisi. Aenean nec enim nec nunc condimentum ultricies.
+                                        Sed nec lacus eget odio fermentum tincidunt. Nulla facilisi.
+                                        Aenean nec enim nec nunc condimentum ultricies.
+                                    </p>
+                                </div>
+                            </div>
+                            <div className='welcome-map'>
+                                <div className='welcome-map-content'>
+                                    <img src="/img/nederland-l.png" alt="Map" />
+                                </div>
+                                <div className='welcome-map-text'>
+                                    <h1>
+                                        # beschikbaar in noord Nederland
+                                    </h1>
+                                </div>
+
+                            </div>
                         </section>
                     </main>
                 </div>
